a29cc575a9cb6b6bf9c976f63bd88117758db7ad
CahyaAji <cahya.akk@gmail.com>
2025-02-25 09:13:08 +0700

    add set location button & dialog

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
-document.getElementById("set-loc-btn").addEventListener("click", () => {
-  const lat = prompt("Enter Latitude: ");
-  const lon = prompt("Enter Longitude");
+document.getElementById("submit-set-loc").addEventListener("click", () => {
+  const latitude = document.getElementById("latitude").value;
+  const longitude = document.getElementById("longitude").value;
+  const errorDialog = document.getElementById("error-dialog");
 
-  if (lat && lon) {
-    console.log(`Latitude: ${lat}, Longitude: ${lon}`);
-  } else {
-    console.log("Input was canceled or empty.");
+  if (!/^-?\d+(\.\d+)?$/.test(latitude) || !/^-?\d+(\.\d+)?$/.test(longitude)) {
+    errorDialog.style.display = "block";
+    setTimeout(() => {
+      errorDialog.style.display = "none";
+    }, 5000);
+    return;
   }
+  window.NodeFn.setLocation(latitude, longitude);
+});
+
+document.getElementById("set-loc-btn").addEventListener("click", () => {
+  const setLocDialog = document.querySelector(".set-loc-dialog");
+
+  if (setLocDialog.style.display === "flex") {
+    setLocDialog.style.display = "none";
+  } else {
+    setLocDialog.style.display = "flex";
+  }
+});
+
+document.getElementById("cancel-set-loc").addEventListener("click", () => {
+  document.querySelector(".set-loc-dialog").style.display = "none";
 });
